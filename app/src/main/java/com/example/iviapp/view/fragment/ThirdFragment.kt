3368b5dd7543a2ece91cc5852228763809a5f15b
Zhasaral Almabek <thunderccloud@gmail.com>
2020-04-30 10:36:51 +0600

    added fb analytics and fb crashlytics

--- app/src/main/java/com/example/iviapp/view/fragment/ThirdFragment.kt
+++ app/src/main/java/com/example/iviapp/view/fragment/ThirdFragment.kt
@@ -13,11 +13,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.crashlytics.android.Crashlytics
 import com.example.iviapp.R
 import com.example.iviapp.view.activity.MainActivity
 import com.example.iviapp.model.account.CurrentUser
 import com.example.iviapp.view_model.ProfileViewModel
 import com.example.iviapp.view_model.ViewModelProviderFactory
+import com.google.firebase.crashlytics.internal.CrashlyticsNativeComponent
+import com.google.firebase.crashlytics.internal.common.CrashlyticsCore
+import kotlinx.android.synthetic.main.profile_page.view.*
+import okhttp3.internal.userAgent
 
 class ThirdFragment : Fragment() {
     private lateinit var profileViewModel: ProfileViewModel
@@ -35,6 +40,7 @@ class ThirdFragment : Fragment() {
             ) as ViewGroup
         val name: TextView = rootView.findViewById(R.id.name)
         val logoutBtn: Button = rootView.findViewById(R.id.logout)
+        val crashBtn: Button = rootView.findViewById(R.id.crash)
         name.text = CurrentUser.user.userName
         progressBar = rootView.findViewById(R.id.progressBar)
 
@@ -67,6 +73,23 @@ class ThirdFragment : Fragment() {
 
         logoutBtn.setOnClickListener {
             profileViewModel.logout(rootView)
+        }
+
+        crashBtn.setOnClickListener {
+            Crashlytics.getInstance().crash()
+            Crashlytics.setUserIdentifier("user1")
+        }
+
+        fun setKey(key: String){
+            Crashlytics.setString(key, "foo")
+
+            Crashlytics.setBool(key, true )
+
+            Crashlytics.setDouble(key, 1.0)
+
+            Crashlytics.setFloat(key, 1.0f)
+
+            Crashlytics.setInt(key, 1)
         }
         return rootView
     }
